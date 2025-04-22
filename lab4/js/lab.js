6f8fd5fcc8ea36c013abf94ddef12d365d96ed9f
waluggy <35122176+waluggy@users.noreply.github.com>
2025-04-21 17:12:27 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -17,5 +17,22 @@ function main() {
   // the code that makes everything happen
 }
 
+//Rock_Paper_Scissors
+//An implementation of the game
+//Ally Herro <aherro@ucsc.edu>
+//April 2025
+
 // let's get this party started
 main();
+
+//Task: Rock-Paper-Scissors
+  //Put fist on hand
+    //Chant “rock, paper, scissors, shoot,” and play hand on “shoot”
+      //Rock hand beats scissors
+      //Scissors hand beats paper
+      //Paper hand beats scissors
+  //Hands must be played simultaneously with opponent
+  //Evaluate played hands
+    //Win condition: hand beats opponent’s hand
+    //Draw condition: hand is the same as opponent’s
+      //Play again
