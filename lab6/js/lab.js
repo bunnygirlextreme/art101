3da709de1a8c28b69afaa018828f6fa0b9ff4396
waluggy <35122176+waluggy@users.noreply.github.com>
2025-04-28 23:32:25 -0700

    lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Ally Herro <aherro@ucsc.edu>
+// Date: 28 April 2025
 
 // Constants
 
@@ -19,3 +19,28 @@ function main() {
 
 // let's get this party started
 main();
+
+// index.js - arrays and objects
+// author: Ally Herro <aherro@ucsc.edu>
+// date: 28 April 2025
+
+// define variables
+myTransport = ["walking", "bus", "bumming a ride", "imaginary VW Beetle from last week"];
+
+// create an object for myMainRide
+myMainRide = {
+  make: "Volkswagen",
+  model: "Beetle",
+  color: "brown",
+  year: 2013,
+  age: function() {
+    return 2025 - this.year;
+  }
+}
+
+// output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '/t'), "</pre>"
+);
